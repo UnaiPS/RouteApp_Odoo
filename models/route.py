6a8547b46a71e3da5d51fc81cdb91b7198a0d4bd
daira <dairaegusquiza@gmail.com>
2020-01-29 13:23:57 +0100

    Not funcional but complete version

--- models/route.py
+++ models/route.py
@@ -1,5 +1,5 @@
 from openerp import models, fields
-#from odoo import exceptions
+from odoo import exceptions
 
 class Route(models.Model):
     _name = 'routes.route'
@@ -11,20 +11,20 @@ class Route(models.Model):
     product_ids = fields.Many2many('product.product', string="Product/Service", required=True)
     distance = fields.Float()
     time = fields.Float()
-#    speed = fields.Char()
+    speed = fields.Char()
     completed = fields.Boolean("Completed", default=False, required=True)
 
-#@api.constrains('origin_id', 'destination_ids')
-#def _check_origin_not_destination(self):
-#    for r in self:
-#        if r.origin_id and r.origin_id in r.destination_ids:
-#            raise exceptions.Warning("A route's origin cannot be a destination")
+@api.constrains('origin_id', 'destination_ids')
+def _check_origin_not_destination(self):
+    for r in self:
+        if r.origin_id and r.origin_id in r.destination_ids:
+            raise exceptions.Warning("A route's origin cannot be a destination")
 
-#@api.onchange('distance', 'time')
-#def _onchange_speed:
-#    if self.distance / self.time >= 0.7 and self.distance / self.time <= 1.25:
-#        self.speed = "normal"
-#    elif self.distance / self.time < 0.7:
-#        self.speed = "slow"
-#    else:
-#        self.speed = "fast"
+@api.onchange('distance', 'time')
+def _onchange_speed():
+    if self.distance / self.time >= 0.7 and self.distance / self.time <= 1.25:
+        self.speed = "normal"
+    elif self.distance / self.time < 0.7:
+        self.speed = "slow"
+    else:
+        self.speed = "fast"
--- models/routeuser.py
+++ models/routeuser.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 class Routeuser(models.Model):
     _name = 'routes.routeuser'
-    _inherit ='res.users'
+    _inherit ='res.user'
     route_id = fields.One2many('routes.route', string="route")
     admin = fields.Boolean("Admin", default=False)
     
